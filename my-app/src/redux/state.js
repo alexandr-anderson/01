689f08cb12fd6d87749026e31b8ee995838331f0
alexandr-anderson <79601499+alexandr-anderson@users.noreply.github.com>
2021-03-18 11:02:32 +0300

    add post action create update new post text action create

--- my-app/src/redux/state.js
+++ my-app/src/redux/state.js
@@ -1,4 +1,5 @@
-import {act} from "@testing-library/react";
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 let store = {
     _state: {
@@ -59,7 +60,10 @@ let store = {
             this._callSubscriber(this._state);
         }
     }
-
 }
+export const addPostActionCreator = () => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (text) => ({
+        type: UPDATE_NEW_POST_TEXT, newText: text})
+
 export default store
 window.store = store;
