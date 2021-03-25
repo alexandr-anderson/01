93af54021587ffc2e076089fd392fa408341b902
alexandr-anderson <79601499+alexandr-anderson@users.noreply.github.com>
2021-03-25 18:26:04 +0300

    axios

--- my-app/src/components/Users/Users.js
+++ my-app/src/components/Users/Users.js
@@ -1,52 +1,20 @@
 import React from 'react'
 import styles from './Users.module.css';
+import * as axios from "axios";
 
 let Users = (props) => {
-
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    photoUrl: 'https://image.flaticon.com/icons/png/512/147/147144.png',
-                    followed: false,
-                    fullName: 'Dmitriy',
-                    status: 'I am a boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://image.flaticon.com/icons/png/512/147/147144.png',
-                    followed: true,
-                    fullName: 'Alexandr',
-                    status: 'I am a boss too',
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://image.flaticon.com/icons/png/512/147/147144.png',
-                    followed: false,
-                    fullName: 'Andrew',
-                    status: 'I am a boss too',
-                    location: {city: 'Kiev', country: 'Ukraine'}
-                },
-                {
-                    id: 4,
-                    photoUrl: 'https://image.flaticon.com/icons/png/512/147/147144.png',
-                    followed: false,
-                    fullName: 'Sue',
-                    status: 'I am a boss too',
-                    location: {city: 'London', country: 'Great Britain'}
-                },
-            ]
-        )
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items)
+            })
     }
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
@@ -60,12 +28,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
